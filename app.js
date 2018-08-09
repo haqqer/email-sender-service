22514f181b951e4a78c92f88a803c4ebbaa91e59
wkwksama <mailpakgasing@gmail.com>
2018-08-09 10:14:38 +0700

    exceed limit request

--- app.js
+++ app.js
@@ -31,8 +31,8 @@ app.set('view engine', 'hbs');
 
 app.use(cors())
 app.use(logger('dev'));
-app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
+app.use(express.json({limit: '50mb'}));
+app.use(express.urlencoded({ limit: '50mb', extended: true }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
