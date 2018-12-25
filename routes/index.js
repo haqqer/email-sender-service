ad5acb38030000ac0c691c9fbe7d671e39554328
wkwksama <mailpakgasing@gmail.com>
2018-12-25 14:44:19 +0700

    naming controller test

--- routes/index.js
+++ routes/index.js
@@ -1,5 +1,5 @@
 const router = require('express').Router();
-const test = require('../controllers')
+const test = require('../controllers/test')
 
 /* GET home page. */
 router.get('/', test.hello);
