0e3d82a43798052744d68c784a31225ebdd708dc
wkwksama <mailpakgasing@gmail.com>
2018-12-26 16:48:56 +0700

    handle multiple email destination

--- services/email.service.js
+++ services/email.service.js
@@ -3,17 +3,25 @@ const Tracking = require('../models/Tracking')
 const { objectKeysToCamelCase } = require('../utils')
 
 const send = async ({ to, subject, html }) => {
-  const data = {
-    from: process.env.MAILGUN_EMAIL_SENDER,
-    to,
-    subject,
-    html
-  };
+  to = to.replace(/\s+/g, ''); // remove every space
+  to = to.replace(/,\s*$/, ""); // remove last coma and space if there trailing
+  to = to.split(',') // finally make it as an array of address
+
+  const mails = to.map(mail => {
+    return {
+      from: process.env.MAILGUN_EMAIL_SENDER,
+      to: mail,
+      subject,
+      html
+    }
+  })
 
   try {
-    const response = await mailgun.messages().send(data);
-    console.log('berhasil', response)
-    return response
+    for (const mail of mails) {
+      const response = await mailgun.messages().send(mail);
+      console.log('>>> mengirim...\n', response)
+    }
+    return 'sent'
   } catch (error) {
     console.log('xxx error \n', error)
     return error
