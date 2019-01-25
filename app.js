f30d9cfa84ed5e43a242a31d3b2c11f753162153
wkwksama <mailpakgasing@gmail.com>
2019-01-26 02:02:00 +0700

    lint with standard

--- app.js
+++ app.js
@@ -1,55 +1,55 @@
-const createError = require('http-errors');
-const express = require('express');
-const path = require('path');
-const cookieParser = require('cookie-parser');
-const logger = require('morgan');
-const cors = require('cors');
-let mongoose = require('mongoose');
+const createError = require('http-errors')
+const express = require('express')
+const path = require('path')
+const cookieParser = require('cookie-parser')
+const logger = require('morgan')
+const cors = require('cors')
+let mongoose = require('mongoose')
 
 require('dotenv').config()
 
-const routes = require('./app.route');
+const routes = require('./app.route')
 
-const app = express();
+const app = express()
 
 // koneksi mongo
 let mongoUrl = process.env.MONGO_STRING || 'mongodb://localhost:27017/emailService'
-mongoose.Promise = global.Promise;
+mongoose.Promise = global.Promise
 mongoose.connect(mongoUrl, { useNewUrlParser: true }, function (err) {
   if (err) {
     console.log(err)
   } else {
     console.log('mongo connected', mongoUrl)
   }
-});
+})
 
 // view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'hbs');
+app.set('views', path.join(__dirname, 'views'))
+app.set('view engine', 'hbs')
 
 app.use(cors())
-app.use(logger('dev'));
-app.use(express.json({limit: '50mb'}));
-app.use(express.urlencoded({ limit: '50mb', extended: true }));
-app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(logger('dev'))
+app.use(express.json({ limit: '50mb' }))
+app.use(express.urlencoded({ limit: '50mb', extended: true }))
+app.use(cookieParser())
+app.use(express.static(path.join(__dirname, 'public')))
 
-app.use('/', routes);
+app.use('/', routes)
 
 // catch 404 and forward to error handler
-app.use(function(req, res, next) {
-  next(createError(404));
-});
+app.use(function (req, res, next) {
+  next(createError(404))
+})
 
 // error handler
-app.use(function(err, req, res, next) {
+app.use(function (err, req, res, next) {
   // set locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
+  res.locals.message = err.message
+  res.locals.error = req.app.get('env') === 'development' ? err : {}
 
   // render the error page
-  res.status(err.status || 500);
-  res.render('error');
-});
+  res.status(err.status || 500)
+  res.render('error')
+})
 
-module.exports = app;
+module.exports = app
--- app.route.js
+++ app.route.js
@@ -1,13 +1,13 @@
-const router = require('express').Router();
+const router = require('express').Router()
 const delivery = require('./lib/delivery/delivery.controller')
 const tracking = require('./lib/tracking/tracking.controller')
 
 /* GET home page. */
 router.get('/', (req, res, next) => {
-  res.render('index', { title: 'Express' });
-});
+  res.render('index', { title: 'Express' })
+})
 
-router.post('/delivery', delivery.send);
-router.post('/webhook/tracking', tracking.handleEvent);
+router.post('/delivery', delivery.send)
+router.post('/webhook/tracking', tracking.handleEvent)
 
-module.exports = router;
+module.exports = router
