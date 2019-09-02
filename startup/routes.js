cc072015b4974901bcd5527ad51b9f59821db4dd
haqqer <ilhamserbaguna@gmail.com>
2019-09-02 21:09:34 +0700

    fix es lint

--- startup/routes.js
+++ startup/routes.js
@@ -1,11 +1,11 @@
 const delivery = require('../lib/delivery/delivery.route')
 const tracking = require('../lib/tracking/tracking.route')
-const email_template = require('../lib/tracking/email_template.route')
+const emailTemplate = require('../lib/tracking/email_template.route')
 
 module.exports = function (app) {
   app.use('/deliveries', delivery)
   app.use('/trackings', tracking)
-  app.use('/email_template', email_template)
+  app.use('/emailtemplate', emailTemplate)
   app.use('/', function (req, res) {
     res.render('index', { title: 'Halo' })
   })
